733088f89ecef56a6bff0cc4e91c9ea8d466d3f2
verycoolusername789 <154019724+verycoolusername789@users.noreply.github.com>
2024-01-11 10:14:53 -0500

    installed tailwind css and created config file (+ comments)
    need to configure more for css (figure that out)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,11 +10,12 @@ export default function Page(){
             {/* change this into a component */}
             <h2>Progress Bar:</h2>
             <p>
-                {/* change this info stuff into??? a component??? also make all this dynamic */}
+                {/* change this info stuff into??? a component??? maybe?? also make all this dynamic */}
                 Info placeholder:
             </p>
             <h3>Past Entries: </h3>
             <p>Table placeholder:</p>
+            {/* make this button do stuff */}
             <button>Sign Out</button>
         </div>
     </main>
